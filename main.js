8dfd06b2a233e05c3ef6f7239affaf6fa88730ab
Robert Howington <zerorob2787@gmail.com>
2017-07-11 16:40:45 -0400

    initial commit - first pass, completed minimum - come back for icons

---
+++ main.js
@@ -0,0 +1,169 @@
+// The Form Data
+let formData = [
+  // For demonstration purposes,
+  // the first form element has been added to the HTML file as a comment
+  // compare the input in the HTML file with the contents of this first object
+  {
+    "type": "text",
+    "label": "First Name",
+    "id": "user-first-name",
+    "icon": "fa-user",
+    "options": []
+  },
+  {
+    "type": "text",
+    "label": "Last Name",
+    "id": "user-last-name",
+    "icon": "fa-user",
+    "options": []
+  },
+  {
+    "type": "email",
+    "label": "Email Address",
+    "id": "user-email",
+    "icon": "fa-envelope",
+    "options": []
+  },
+  {
+    "type": "text",
+    "label": "Current Website URL",
+    "id": "user-website",
+    "icon": "fa-globe",
+    "options": []
+  },
+  {
+    "type": "select",
+    "label": "Select Language",
+    "id": "user-language",
+    "icon": "",
+    "options": [{
+        "label": "English",
+        "value": "EN"
+      },
+      {
+        "label": "French",
+        "value": "FR"
+      },
+      {
+        "label": "Spanish",
+        "value": "SP"
+      },
+      {
+        "label": "Chinese",
+        "value": "CH"
+      },
+      {
+        "label": "Japanese",
+        "value": "JP"
+      }
+    ]
+  },
+  {
+    "type": "textarea",
+    "label": "Your Comment",
+    "id": "user-comment",
+    "icon": "fa-comments",
+    "options": []
+  },
+  {
+    "type": "tel",
+    "label": "Mobile Number",
+    "id": "user-mobile",
+    "icon": "fa-mobile-phone",
+    "options": []
+  },
+  {
+    "type": "tel",
+    "label": "Home Number",
+    "id": "user-phone",
+    "icon": "fa-phone",
+    "options": []
+  }
+];
+
+// HINTS:
+// As you can see, we access the first element in the array
+// with [0] and then grab the property "label" using the "." operator
+(function() {
+  // Select the first element from the array
+  let first = formData[0];
+  // Log the first object
+  console.log(first);
+  // Log the string "First Name"
+  console.log(first.label);
+})();
+
+
+// -------- Your Code Goes Below this Line --------
+
+let formField = document.getElementById("fields");
+
+for (var i = 0; i < formData.length; i++) {
+
+  let input = document.createElement('input');
+  let textarea = document.createElement('textarea');
+  let select = document.createElement('select');
+
+
+
+  function setAttributes(el, attrs) {
+    for (var key in attrs) {
+      el.setAttribute(key, attrs[key]);
+    }
+  }
+
+  if (formData[i].type !== "select" && formData[i].type !== "textarea") {
+
+    formField.appendChild(input);
+
+    setAttributes(input, {
+      "type": formData[i].type,
+      "id": formData[i].id,
+      "placeholder": formData[i].label,
+      "icon": formData[i].icon
+    })
+  } else if (formData[i].type === "select") {
+
+    let defaultText = document.createElement("option");
+    defaultText.setAttribute("label", "Select language...");
+    select.appendChild(defaultText);
+
+    setAttributes(select, {
+      "type": formData[i].type,
+      "id": formData[i].id,
+      "label": formData[i].label,
+      "icon": formData[i].icon,
+      "options": formData[i].options
+    });
+
+    for (var j = 0; j < formData[i].options.length; j++) {
+      let options = document.createElement("option")
+      options.setAttribute("label",formData[i].options[j].label);
+      options.setAttribute("value",formData[i].options[j].value);
+      select.appendChild(options);
+      formField.appendChild(select);
+    }
+    formField.appendChild(select);
+
+    //Looping through options array for the select field
+
+  } else {
+    //Textarea
+    formField.appendChild(textarea);
+
+    setAttributes(textarea, {
+      "type": formData[i].type,
+      "id": formData[i].id,
+      "placeholder": formData[i].label,
+      "icon": formData[i].icon
+    });
+
+  };
+
+
+
+
+
+
+
+}
